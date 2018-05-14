b71151743ced041b8e3035a84ad2623a26b39fcf
Ecialo <ecialo@yandex.ru>
2018-05-14 18:19:51 +0300

    Funcs

--- mlp/grid.py
+++ mlp/grid.py
@@ -55,6 +55,9 @@ class FixedArea:
     def __repr__(self):
         return str(self.cells)
 
+    def length(self):
+        return len(self)
+
     def is_empty(self):
         return all((cell.object is None for cell in self.cells))
 
